b617fed1ba0f30317e5557612053d70c5cf022ac
Dennis Kreußel <dennis.kreussel@deepl.com>
2024-05-11 21:16:25 +0200

    auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,1 +1,24 @@
+// Package auth defines the authentication layer of the application.
 package auth
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/denniskreussel/scm/internal/jwt"
+)
+
+// Logout removes session cookies and redirect to home.
+func Logout(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(jwt.TokenCookieKey)
+	if err != nil {
+		// Ignore error. Cookie doesn't exists.
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	cookie.Value = ""
+	cookie.Path = "/"
+	cookie.Expires = time.Now().Add(-1 * time.Hour)
+	http.SetCookie(w, cookie)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
